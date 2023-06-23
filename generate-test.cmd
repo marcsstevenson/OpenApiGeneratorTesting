3057af6397daa4d4a9a7776fa1498ca94d406658
Marc Stevenson <marcsstevenson@hotmail.com>
2023-06-23 16:30:30 +1200

    Init

---
+++ generate-test.cmd
@@ -0,0 +1,14 @@
+@REM powershell -Command "(gc code-gen-config-template-local.yaml -Raw) -replace '__packageName__', '%packageNameSpace%' | Out-File -encoding ASCII %codeGenConfigFile%"
+
+call openapi-generator-cli generate ^
+-g aspnetcore ^
+-i event-api-v1.swagger.3.0.3.yaml ^
+-o output ^
+--additional-properties=^
+buildTarget=library^
+,packageName=OpenApiGeneratorTesting^
+,operationResultTask=true^
+,useCollection=true^
+,useSwashbuckle=true^
+,nullableReferenceTypes=true ^
+--global-property models,apis,supportingFiles=CustomEnumConverter.cs:OnlyStringEnumConverter.cs, ^
